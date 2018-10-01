6e343a62446594ca96075e0d0a66f0e1440fa574
sapjpp <sap.jpp@gmail.com>
2018-10-01 14:34:01 -0500

    no actual change

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -5,6 +5,7 @@ public class FizzBuzz {
     public String play(int number) {
 
         if (number == 0) throw new IllegalArgumentException("Number must not be 0");
+        // dummy change
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
 
